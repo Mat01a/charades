2e1d6dabd8cbcc078195c3da9d6c4d67dd8b936f
Mateusz <matolszakowski@gmail.com>
2026-02-13 23:01:21 +0100

    feat: Add cache settings

--- puns_game/settings.py
+++ puns_game/settings.py
@@ -93,6 +93,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
+
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
