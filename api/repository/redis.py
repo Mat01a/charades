a556fe4a3628aa96dea7c75360392a429f4a23a2
Mateusz <matolszakowski@gmail.com>
2026-02-14 15:48:23 +0100

    feat: Add has function in repository and add repository test

--- api/repository/redis.py
+++ api/repository/redis.py
@@ -2,7 +2,7 @@ from django_redis import get_redis_connection
 
 class Redis:
 
-    def __init__(self, type):
+    def __init__(self, type="default"):
         self.connection = get_redis_connection(type)
 
     def save(self, set_key, members):
@@ -11,3 +11,6 @@ class Redis:
     def get(self, members):
         results = self.connection.smembers(members)
         return results
+    
+    def has(self, set_key, member):
+        return self.connection.sismember(set_key, member)
--- api/tests/test_game_repository.py
+++ api/tests/test_game_repository.py
@@ -3,7 +3,7 @@ from api.repository.redis import Redis
 
 class RepositoryTest(TestCase):
     def setUp(self):
-        self.db = Redis()
+        self.db = Redis("tests")
     
     def test_valid_saving(self):
         """Testing saving function while input is valid"""
@@ -11,8 +11,13 @@ class RepositoryTest(TestCase):
         self.assertIsNone(result)
 
     def test_invalid_params_saving_raises_error(self):
-        """Testing saving with invalid input """
+        """Testing saving with invalid input"""
         with self.assertRaises(TypeError) as context:
             invalid_amount_of_params = self.db.save("lack_of_second_argument")
             too_much_params = self.db.save("too","much","arguments")
-        
+    
+    def test_has_function(self):
+        """Testing has function"""
+        self.db.save(f"room:1:state", "active")
+        has = self.db.has(f"room:1:state", "active")
+        self.assertEqual(has, 1)
