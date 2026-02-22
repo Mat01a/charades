043cb942fa50ca7be46dfacedcfbcec277e9761d
Mateusz <matolszakowski@gmail.com>
2026-02-22 19:55:51 +0100

    refactore: Title test splitting.

--- frontend/playwright/tests/username.spec.ts
+++ frontend/playwright/tests/username.spec.ts
@@ -1,10 +1,5 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-    await page.goto('')
-
-    await expect(page).toHaveTitle('Charades')
-})
 
 test('input username', async ({ page}) => {
     await page.goto('')
